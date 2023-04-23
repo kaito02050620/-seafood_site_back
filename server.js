0daaa6ee318824ddd1fbca9876edfdbce13b4dd7
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-23 18:17:36 +0900

    cors再設定

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://seafoodcook.netlify.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
